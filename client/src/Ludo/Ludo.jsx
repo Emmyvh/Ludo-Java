3efc16dfacb39c11a22d11fc42d195182b53fac3
Emmyvh <97029433+Emmyvh@users.noreply.github.com>
2022-03-28 16:24:02 +0200

    Added client functionality. Still chasing a domain bug.

--- client/src/Ludo/Ludo.jsx
+++ client/src/Ludo/Ludo.jsx
@@ -6,10 +6,10 @@ import Three from "./three.png";
 import Four from "./four.png";
 import Five from "./five.png";
 import Six from "./six.png";
-import PawnOne from "./PawnOne.png";
-import PawnTwo from "./PawnTwo.png";
-import PawnThree from "./PawnThree.png";
-import PawnFour from "./PawnFour.png";
+import P1Pawn from "./P1Pawn.png";
+import P2Pawn from "./P2Pawn.png";
+import P3Pawn from "./P3Pawn.png";
+import P4Pawn from "./P4Pawn.png";
 
 export function PlayLudo() {
 
@@ -87,38 +87,152 @@ export function PlayLudo() {
     }
 
     function DiceImage() {
-        if (newBoard) {
-            let list = [One, Two, Three, Four, Five, Six]
-            return list[newBoard.diceThrow - 1]
+        let list = [One, Two, Three, Four, Five, Six]
+        return list[newBoard.diceThrow - 1]
+    }
+
+    function pawnPicture() {
+        if (activePlayer == "player one") {
+            return P1Pawn;
+        } else if (activePlayer == "player two") {
+            return P2Pawn;
+        } else if (activePlayer == "player three") {
+            return P3Pawn;
+        } else if (activePlayer == "player four") {
+            return P4Pawn;
+        }
+    }
+
+    function occupiedSquare(fieldIndex) {
+        if (newBoard.field[fieldIndex].occupied) {
+            if (newBoard.playerOne.pawnList.includes(fieldIndex)) {
+                return "occupiedOne";
+            } else if (newBoard.playerTwo.pawnList.includes(fieldIndex)) {
+                return "occupiedTwo";
+            } else if (newBoard.playerThree.pawnList.includes(fieldIndex)) {
+                return "occupiedThree";
+            } else if (newBoard.playerFour.pawnList.includes(fieldIndex)) {
+                return "occupiedFour";
+            }
+        } else {
+            return "square"
+        }
+    }
+
+    function occSquareOne(fieldIndex) {
+        if (newBoard.field[fieldIndex].occupied) {
+            if (newBoard.playerOne.pawnList.includes(fieldIndex)) {
+                return "occupiedOne";
+            } else if (newBoard.playerTwo.pawnList.includes(fieldIndex)) {
+                return "occupiedTwo";
+            } else if (newBoard.playerThree.pawnList.includes(fieldIndex)) {
+                return "occupiedThree";
+            } else if (newBoard.playerFour.pawnList.includes(fieldIndex)) {
+                return "occupiedFour";
+            }
+        } else {
+            return "startSquareOne"
+        }
+    }
+
+    function occSquareTwo(fieldIndex) {
+        if (newBoard.field[fieldIndex].occupied) {
+            if (newBoard.playerOne.pawnList.includes(fieldIndex)) {
+                return "occupiedOne";
+            } else if (newBoard.playerTwo.pawnList.includes(fieldIndex)) {
+                return "occupiedTwo";
+            } else if (newBoard.playerThree.pawnList.includes(fieldIndex)) {
+                return "occupiedThree";
+            } else if (newBoard.playerFour.pawnList.includes(fieldIndex)) {
+                return "occupiedFour";
+            }
+        } else {
+            return "startSquareTwo"
+        }
+    }
+
+    function occSquareThree(fieldIndex) {
+        if (newBoard.field[fieldIndex].occupied) {
+            if (newBoard.playerOne.pawnList.includes(fieldIndex)) {
+                return "occupiedOne";
+            } else if (newBoard.playerTwo.pawnList.includes(fieldIndex)) {
+                return "occupiedTwo";
+            } else if (newBoard.playerThree.pawnList.includes(fieldIndex)) {
+                return "occupiedThree";
+            } else if (newBoard.playerFour.pawnList.includes(fieldIndex)) {
+                return "occupiedFour";
+            }
+        } else {
+            return "startSquareThree"
+        }
+    }
+
+    function occSquareFour(fieldIndex) {
+        if (newBoard.field[fieldIndex].occupied) {
+            if (newBoard.playerOne.pawnList.includes(fieldIndex)) {
+                return "occupiedOne";
+            } else if (newBoard.playerTwo.pawnList.includes(fieldIndex)) {
+                return "occupiedTwo";
+            } else if (newBoard.playerThree.pawnList.includes(fieldIndex)) {
+                return "occupiedThree";
+            } else if (newBoard.playerFour.pawnList.includes(fieldIndex)) {
+                return "occupiedFour";
+            }
+        } else {
+            return "startSquareFour"
+        }
+    }
+
+    function PawnOne() {
+        if (activePlayer == "player one") {
+            return Number(newBoard.playerOne.pawnList[0]) + 1;
+        } else if (activePlayer == "player two") {
+            return Number(newBoard.playerTwo.pawnList[0]) + 1;
+        } else if (activePlayer == "player three") {
+            return Number(newBoard.playerThree.pawnList[0]) + 1;
+        } else if (activePlayer == "player four") {
+            return Number(newBoard.playerFour.pawnList[0]) + 1;
+        }
+    }
+
+    function PawnTwo() {
+        if (activePlayer == "player one") {
+            return Number(newBoard.playerOne.pawnList[1]) + 1;
+        } else if (activePlayer == "player two") {
+            return Number(newBoard.playerTwo.pawnList[1]) + 1;
+        } else if (activePlayer == "player three") {
+            return Number(newBoard.playerThree.pawnList[1]) + 1;
+        } else if (activePlayer == "player four") {
+            return Number(newBoard.playerFour.pawnList[1]) + 1;
+        }
+    }
+
+    function PawnThree() {
+        if (activePlayer == "player one") {
+            return Number(newBoard.playerOne.pawnList[2]) + 1;
+        } else if (activePlayer == "player two") {
+            return Number(newBoard.playerTwo.pawnList[2]) + 1;
+        } else if (activePlayer == "player three") {
+            return Number(newBoard.playerThree.pawnList[2]) + 1;
+        } else if (activePlayer == "player four") {
+            return Number(newBoard.playerFour.pawnList[2]) + 1;
+        }
+    }
+
+    function PawnFour() {
+        if (activePlayer == "player one") {
+            return Number(newBoard.playerOne.pawnList[3]) + 1;
+        } else if (activePlayer == "player two") {
+            return Number(newBoard.playerTwo.pawnList[3]) + 1;
+        } else if (activePlayer == "player three") {
+            return Number(newBoard.playerThree.pawnList[3]) + 1;
+        } else if (activePlayer == "player four") {
+            return Number(newBoard.playerFour.pawnList[3]) + 1;
         }
     }
 
     function winner() {
         if (gameStatus.gameStatus) { alert("The winner is " + gameStatus.winner) }
-    }
-
-    // predefined variables for the return.
-    if (newBoard) {
-
-        var playerOnePawnOneLocation = Number(newBoard.playerOne.pawnList[0]) + 1;
-        var playerOnePawnTwoLocation = Number(newBoard.playerOne.pawnList[1]) + 1;
-        var playerOnePawnThreeLocation = Number(newBoard.playerOne.pawnList[2]) + 1;
-        var playerOnePawnFourLocation = Number(newBoard.playerOne.pawnList[3]) + 1;
-
-        var playerTwoPawnOneLocation = Number(newBoard.playerTwo.pawnList[0]) + 1;
-        var playerTwoPawnTwoLocation = Number(newBoard.playerTwo.pawnList[1]) + 1;
-        var playerTwoPawnThreeLocation = Number(newBoard.playerTwo.pawnList[2]) + 1;
-        var playerTwoPawnFourLocation = Number(newBoard.playerTwo.pawnList[3]) + 1;
-
-        var playerThreePawnOneLocation = Number(newBoard.playerThree.pawnList[0]) + 1;
-        var playerThreePawnTwoLocation = Number(newBoard.playerThree.pawnList[1]) + 1;
-        var playerThreePawnThreeLocation = Number(newBoard.playerThree.pawnList[2]) + 1;
-        var playerThreePawnFourLocation = Number(newBoard.playerThree.pawnList[3]) + 1;
-
-        var playerFourPawnOneLocation = Number(newBoard.playerFour.pawnList[0]) + 1;
-        var playerFourPawnTwoLocation = Number(newBoard.playerFour.pawnList[1]) + 1;
-        var playerFourPawnThreeLocation = Number(newBoard.playerFour.pawnList[2]) + 1;
-        var playerFourPawnFourLocation = Number(newBoard.playerFour.pawnList[3]) + 1;
     }
 
     // The layout of the board.
@@ -129,120 +243,115 @@ export function PlayLudo() {
                     <button className="startOver" onClick={() => StartLudo()}>Restart game</button>
                     <div className="dice"><img src={DiceImage()} /></div>
                     <button className="addPawn" onClick={() => PlacePawn()}>Place a pawn</button>
-                    <button className="movePawn" onClick={() => MovePawn(1)}><img src={PawnOne} /></button>
-                    <button className="movePawn" onClick={() => MovePawn(2)}><img src={PawnTwo} /></button>
-                    <button className="movePawn" onClick={() => MovePawn(3)}><img src={PawnThree} /></button>
-                    <button className="movePawn" onClick={() => MovePawn(4)}><img src={PawnFour} /></button>
+                    <button className="movePawn" onClick={() => MovePawn(1)}><img src={pawnPicture()} /><br></br>{PawnOne()} </button>
+                    <button className="movePawn" onClick={() => MovePawn(2)}><img src={pawnPicture()} /><br></br>{PawnTwo()} </button>
+                    <button className="movePawn" onClick={() => MovePawn(3)}><img src={pawnPicture()} /><br></br>{PawnThree()} </button>
+                    <button className="movePawn" onClick={() => MovePawn(4)}><img src={pawnPicture()} /><br></br>{PawnFour()} </button>
                 </div>
 
                 <div className="controls">
-                    <div className="scoreBoard">Score player one: {newBoard.playerOne.score}</div>
-                    <div className="scoreBoard">Score player two: {newBoard.playerTwo.score}</div>
-                    <div className="scoreBoard">Score player three: {newBoard.playerThree.score}</div>
-                    <div className="scoreBoard">Score player four: {newBoard.playerFour.score}</div>
-                </div>
-
-                <div className="textBox">The current player is: {activePlayer}</div>
-                <div className="textBox">Player one's pawns are at the following locations: {playerOnePawnOneLocation}, {playerOnePawnTwoLocation}, {playerOnePawnThreeLocation}, {playerOnePawnFourLocation}</div>
-                <div className="textBox">Player two's pawns are at the following locations: {playerTwoPawnOneLocation}, {playerTwoPawnTwoLocation}, {playerTwoPawnThreeLocation}, {playerTwoPawnFourLocation}</div>
-                <div className="textBox">Player three's pawns are at the following locations: {playerThreePawnOneLocation}, {playerThreePawnTwoLocation}, {playerThreePawnThreeLocation}, {playerThreePawnFourLocation}</div>
-                <div className="textBox">Player four's pawns are at the following locations: {playerFourPawnOneLocation}, {playerFourPawnTwoLocation}, {playerFourPawnThreeLocation}, {playerFourPawnFourLocation}</div>
-
-                <div className="box">
-                    <div className="startSquareTwo">11</div>
-                    <div className="square">12</div>
-                    <div className="square">13</div>
+                    <div className="scoreBoardOne">Score player one: {newBoard.playerOne.score}</div>
+                    <div className="scoreBoardTwo">Score player two: {newBoard.playerTwo.score}</div>
+                    <div className="scoreBoardThree">Score player three: {newBoard.playerThree.score}</div>
+                    <div className="scoreBoardFour">Score player four: {newBoard.playerFour.score}</div>
                 </div>
 
                 <div className="box">
-                    <div className="square">10</div>
-                    <div className="filler"></div>
-                    <div className="square">14</div>
+                    <div className={occSquareTwo(10)}>11</div>
+                    <div className={occupiedSquare(11)} >12</div>
+                    <div className={occupiedSquare(12)}>13</div>
                 </div>
 
                 <div className="box">
-                    <div className="square">09</div>
+                    <div className={occupiedSquare(9)}>10</div>
                     <div className="filler"></div>
-                    <div className="square">15</div>
+                    <div className={occupiedSquare(13)}>14</div>
                 </div>
 
                 <div className="box">
-                    <div className="square">08</div>
+                    <div className={occupiedSquare(8)}>09</div>
                     <div className="filler"></div>
-                    <div className="square">16</div>
+                    <div className={occupiedSquare(14)}>15</div>
                 </div>
 
                 <div className="box">
-                    <div className="square">03</div>
-                    <div className="square">04</div>
-                    <div className="square">05</div>
-                    <div className="square">06</div>
-                    <div className="square">07</div>
+                    <div className={occupiedSquare(7)}>08</div>
                     <div className="filler"></div>
-                    <div className="filler"></div>
-                    <div className="filler"></div>
-                    <div className="square">17</div>
-                    <div className="square">18</div>
-                    <div className="square">19</div>
-                    <div className="square">20</div>
-                    <div className="startSquareThree">21</div>
+                    <div className={occupiedSquare(15)}>16</div>
                 </div>
 
                 <div className="box">
-                    <div className="square">02</div>
+                    <div className={occupiedSquare(2)}>03</div>
+                    <div className={occupiedSquare(3)}>04</div>
+                    <div className={occupiedSquare(4)}>05</div>
+                    <div className={occupiedSquare(5)}>06</div>
+                    <div className={occupiedSquare(6)}>07</div>
                     <div className="filler"></div>
                     <div className="filler"></div>
                     <div className="filler"></div>
-                    <div className="filler"></div>
-                    <div className="filler"></div>
-                    <div className="filler"></div>
-                    <div className="filler"></div>
-                    <div className="filler"></div>
-                    <div className="filler"></div>
-                    <div className="filler"></div>
-                    <div className="filler"></div>
-                    <div className="square">22</div>
+                    <div className={occupiedSquare(16)}>17</div>
+                    <div className={occupiedSquare(17)}>18</div>
+                    <div className={occupiedSquare(18)}>19</div>
+                    <div className={occupiedSquare(19)}>20</div>
+                    <div className={occSquareThree(20)}>21</div>
                 </div>
 
                 <div className="box">
-                    <div className={(newBoard.field[0].isOccupied) ? "occupied" : "startSquareOne"}>01</div>
-                    <div className="square">40</div>
-                    <div className="square">39</div>
-                    <div className="square">38</div>
-                    <div className="square">37</div>
+                    <div className={occupiedSquare(1)}>02</div>
                     <div className="filler"></div>
                     <div className="filler"></div>
                     <div className="filler"></div>
-                    <div className="square">27</div>
-                    <div className="square">26</div>
-                    <div className="square">25</div>
-                    <div className="square">24</div>
-                    <div className="square">23</div>
+                    <div className="filler"></div>
+                    <div className="filler"></div>
+                    <div className="filler"></div>
+                    <div className="filler"></div>
+                    <div className="filler"></div>
+                    <div className="filler"></div>
+                    <div className="filler"></div>
+                    <div className="filler"></div>
+                    <div className={occupiedSquare(21)}>22</div>
                 </div>
 
                 <div className="box">
-                    <div className="square">36</div>
+                    <div className={occSquareOne(0)}>01</div>
+                    <div className={occupiedSquare(39)}>40</div>
+                    <div className={occupiedSquare(38)}>39</div>
+                    <div className={occupiedSquare(37)}>38</div>
+                    <div className={occupiedSquare(36)}>37</div>
                     <div className="filler"></div>
-                    <div className="square">28</div>
-                </div>
-                <div className="box">
-                    <div className="square">35</div>
                     <div className="filler"></div>
-                    <div className="square">29</div>
+                    <div className="filler"></div>
+                    <div className={occupiedSquare(26)}>27</div>
+                    <div className={occupiedSquare(25)}>26</div>
+                    <div className={occupiedSquare(24)}>25</div>
+                    <div className={occupiedSquare(23)}>24</div>
+                    <div className={occupiedSquare(22)}>23</div>
                 </div>
 
                 <div className="box">
-                    <div className="square">34</div>
+                    <div className={occupiedSquare(35)}>36</div>
                     <div className="filler"></div>
-                    <div className="square">30</div>
+                    <div className={occupiedSquare(27)}>28</div>
+                </div>
+                <div className="box">
+                    <div className={occupiedSquare(34)}>35</div>
+                    <div className="filler"></div>
+                    <div className={occupiedSquare(28)}>29</div>
                 </div>
 
                 <div className="box">
-                    <div className="square">33</div>
-                    <div className="square">32</div>
-                    <div className="startSquareFour">31</div>
+                    <div className={occupiedSquare(33)}>34</div>
+                    <div className="filler"></div>
+                    <div className={occupiedSquare(29)}>30</div>
+                </div>
+
+                <div className="box">
+                    <div className={occupiedSquare(32)}>33</div>
+                    <div className={occupiedSquare(31)}>32</div>
+                    <div className={occSquareFour(30)}>31</div>
                 </div>
             </div>)
+
             : <div className="controls">
                 <button className="startGame" onClick={() => StartLudo()}>Start a new game</button>
             </div>}
